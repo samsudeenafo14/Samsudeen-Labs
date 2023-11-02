95870c9cffc97841e06286b88bfe457e5b7b6811
samsudeenafo14 <samsudeenafolabi14@gmail.com>
2023-11-02 11:56:32 +0100

    Update sql subqueries.sql

--- Lab week 3/sql subqueries.sql
+++ Lab week 3/sql subqueries.sql
@@ -33,47 +33,75 @@ where actor_id in (select actor_id
                                          
                                          
 #Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
-SELECT 
-  film_id,(SELECT COUNT(inventory_id)
-          FROM inventory
-          WHERE film_id = film.film_id)
-   AS number_of_copies
-FROM film
-WHERE title = 'Hunchback Impossible';
+select
+  film_id,(select COUNT(inventory_id)
+          from inventory
+          where film_id = film.film_id)
+          as number_of_copies
+          from film
+          where title = 'Hunchback Impossible';
 
 
 
 #List all films whose length is longer than the average length of all the films in the Sakila database.
-SELECT  title, length
-FROM film
-        WHERE length > (SELECT AVG(length)
-        from film); 
+select  title, length
+from film
+        where length > (select avg(length)
+					   from film); 
         
 #Use a subquery to display all actors who appear in the film "Alone Trip"
 
 
-SELECT actor_id, first_name, last_name
-FROM actor
-WHERE actor_id IN (SELECT actor_id
-                   FROM film_actor
-				   WHERE film_id = (SELECT film_id
-				   FROM film
-                   WHERE title = 'Alone Trip'));
+select actor_id, first_name, last_name
+from actor
+where actor_id in (select actor_id
+                   from film_actor
+				   where film_id = (select film_id
+				   from film
+                   where title = 'Alone Trip'));
                    
                    
 # Identify all movies categorized as family films.
-SELECT film_id, title, rating
-FROM film
-WHERE film_id IN ( SELECT film_id
-                 FROM film_category
-                 WHERE category_id = (
-                 SELECT category_id
-                 FROM category
-                 WHERE name = 'Family'));
+select film_id, title, rating
+from film
+where film_id in ( select film_id
+                 from film_category
+                 where category_id = (
+                 select category_id
+                 from category
+                 Where name = 'Family'));
+                 
+
+#Retrieve the customer_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. 
+  
+  
+select customer_id, total_amount_spent
+from (select customer_id,
+	 sum(amount) as total_amount_spent
+	 from payment
+     group by customer_id ) as customer_payments
+     where total_amount_spent > (select avg(total_amount_spent)
+     from (select customer_id, SUM(amount) as total_amount_spent
+     from payment
+	 Group by customer_id) as avg_customer_payments);
+     
+     create temporary table temp_taspc as
+     select (select customer_id,
+	 sum(amount) as total_amount_spent
+	 from payment
+     
+     
+     
+	
+     
+     
+     
+     
+     
   
 
 
- 
+
 
                             
                             
